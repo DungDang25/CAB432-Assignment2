68d33ae86d4ff8352611f48faff0487c7ffae27c
DungDang25 <dungdang25@hotmail.com>
2022-10-18 16:31:46 +1000

    Update index.js

--- test-stream/index.js
+++ test-stream/index.js
@@ -5,12 +5,14 @@ console.log(process.env.bearer_token)
 const client = new Client(process.env.bearer_token);
 
 async function main() {
-  const stream = client.tweets.sampleStream({
-    "tweet.fields": ["author_id"],
-  });
-  for await (const tweet of stream) {
-    console.log("Tweet ID:" + tweet.data?.text + "\nText: " + tweet.data?.text + "\n");
-  }
+    const stream = client.tweets.sampleStream({
+        "tweet.fields": ["author_id"],
+    });
+    for await (const tweet of stream) {
+        if (tweet.data?.text.includes("BTS") || tweet.data?.text.includes("bts")) {
+            console.log("Tweet ID:" + tweet.data?.text + "\nText: " + tweet.data?.text + "\n");
+        }
+    }
 }
 
 main();
